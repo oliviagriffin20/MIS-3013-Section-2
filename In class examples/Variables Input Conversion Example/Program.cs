aeb2ad08693d2535313e21d5002fdbb4718568a1
Olivia Griffin <Olivia.G.Griffin-1@ou.edu>
2021-02-01 13:41:28 -0600

    Created intial solution for example problems and a project.

---
+++ In class examples/Variables Input Conversion Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Variables_Input_Conversion_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
